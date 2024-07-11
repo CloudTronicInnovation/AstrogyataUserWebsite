f8ca434864648bd0582e369f531c895331791f6e
MANOJPATIL143 <mrpatil20181@gmail.com>
2024-07-11 17:48:22 +0530

    "Updated baseURL in axiosConfig.js and changed field name from 'duration' to 'totalDuration' in UserChatHistory.js"

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: "https://api.astrogyata.in/api",
   // baseURL: "http://localhost:4000",
   // baseURL:"http://192.168.1.145:4000/"
-  // baseURL: "http://192.168.1.112:4000/"
+  // baseURL: "http://192.168.1.112:5000/"
   // baseURL: "http://api.astrogyata.in:8686",
 });
 export const panchanURL = axios.create({
